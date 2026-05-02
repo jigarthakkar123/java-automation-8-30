678a060208fe36d74ebae3756ebd811646536a3c
jigarthakkar123 <jigar.thakkar.tops@gmail.com>
2026-05-02 09:23:10 +0530

    2-5-2026 Updated

--- ConstructorChaining.java
+++ ConstructorChaining.java
@@ -6,12 +6,21 @@ class A1
 	{
 		System.out.println("A1's Default Constructor");
 	}
+	void show()
+	{
+		System.out.println("Show From A1");
+	}
 }
 class B1 extends A1
 {
 	B1()
 	{
 		System.out.println("B1's Default Constructor");
+	}
+	void show()
+	{
+		super.show();
+		System.out.println("Show From B1");
 	}
 }
 class C1 extends B1
@@ -20,10 +29,16 @@ class C1 extends B1
 	{
 		System.out.println("C1's Default Constructor");
 	}
+	void show()
+	{
+		super.show();
+		System.out.println("Show From C1");
+	}
 }
 public class ConstructorChaining {
 
 	public static void main(String[] args) {
 		C1 c=new C1();
+		c.show();
 	}
 }
